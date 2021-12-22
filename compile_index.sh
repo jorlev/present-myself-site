7969644176506db5babdee99b88bc27291701842
jorlev <83957900+jorlev@users.noreply.github.com>
2021-12-22 11:56:27 +0100

    Update compile_index.sh

--- compile_index.sh
+++ compile_index.sh
@@ -17,8 +17,10 @@ cd ..
 
 jupyter nbconvert --to html --output-dir $OUTPUT_PATH/ $JUPYTER_FOLDER/*.ipynb
 
+echo -e "\n" >> index.md
 for notebook in $OUTPUT_PATH/*.html
 
 do
-  echo "$notebook" | tee -a index.md
+  echo "https://jorlev.github.io/present-myself-site/$notebook" | tee -a index.md
+  echo -e "\n" >> index.md
 done
